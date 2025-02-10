c4811fd2e9551bc0c1eef8504884de9615641f11
KinJx-AI <kin.kjx@gmail.com>
2025-02-10 18:44:37 +0800

    Navbar draft

---
+++ src/app/booking/page.js
@@ -0,0 +1,11 @@
+import TimeSlotPicker from "./picker";
+
+export default function Booking() {
+  return (
+    <div className="">
+      <div className="flex justify-center items-center min-h-screen bg-gray-950">
+        <TimeSlotPicker />
+      </div>
+    </div>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import Navbar from "./components/Navbar";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -20,9 +22,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
       >
-        {children}
+        <Navbar />
+        <main className="">{children}</main>
       </body>
     </html>
   );
